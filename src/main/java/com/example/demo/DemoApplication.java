9b45ed45b91b00d8ae9061d1234e3d23506eeb5f
sujeetyadav <sujeet.y90@gmail.com>
2022-02-19 22:57:30 +0530

    Added Hello World 2 println

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Hello World 2");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
