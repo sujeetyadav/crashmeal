685e3e9ce37c8a338840c9b9c1c1c4bd5a79396c
sujeetyadav <sujeet.y90@gmail.com>
2022-02-19 23:02:38 +0530

    Added TestOne println

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello World 2");
+		System.out.println("TestOne");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
